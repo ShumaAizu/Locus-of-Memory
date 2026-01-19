cb31f6ea2e20235b1fb44e4f8608ef8d0e7de5ab
KajinoHiyori <251312@st.yoshida-g.ac.jp>
2026-01-19 12:34:19 +0900

    light.h / light.cppの追加

---
+++ Locus_of_Memory/light.h
@@ -0,0 +1,21 @@
+//========================================================
+// 
+// 3Dアクションゲーム_MASTER KEY[light.h]
+// 
+//========================================================
+#ifndef _LIGHT_H_
+#define _LIGHT_H_
+#include "main.h"
+
+// マクロ定義
+#define LIGHT_BRIGHTEST			(D3DXCOLOR(1.000f,	1.000f,	1.000f,	1.000f))	// 1番目に明るい光源
+#define LIGHT_BRIGHTER			(D3DXCOLOR(0.800f,	0.800f,	0.800f,	1.000f))	// 2番目に明るい光源
+#define COLOR_BRIGHT			(D3DXCOLOR(0.600f,	0.600f,	0.600f,	1.000f))	// 3番目に明るい光源
+
+
+// プロトタイプ宣言
+void InitLight(void);
+void UninitLight(void);
+void UpdateLight(void);
+void SetLightColors(D3DXCOLOR Brightest, D3DXCOLOR Brighter, D3DXCOLOR Bright);
+#endif
